154f66667ca368d49fbe23e740e3c8d40abe7408
Analu <contato.analu@icloud.com>
2025-03-19 08:15:09 -0300

    creating app w streamlit

--- src/validator.py
+++ src/validator.py
@@ -1,7 +1,7 @@
 from pydantic import BaseModel, Field
 from typing import Optional
 
-class User(BaseModel):
+class PlanilhaAnuncios(BaseModel):
     Organizador: int = Field(..., description="Identificador do organizador")
     Ano_Mes: str = Field(..., description="Ano e mês no formato 'YYYY | Mês'")
     Dia_da_Semana: str = Field(..., description="Dia da semana")
@@ -10,9 +10,9 @@ class User(BaseModel):
     Date: str = Field(..., description="Data no formato YYYY-MM-DD")
     AdSet_name: str = Field(..., description="Nome do conjunto de anúncios")
     Amount_spent: float = Field(..., ge=0, le=1200.00, description="Valor gasto no anúncio (mínimo 0, máximo 600)")
-    Link_clicks: Optional[int] = Field(None, description="Quantidade de cliques no link")
+    Link_clicks: Optional[float] = Field(None, description="Quantidade de cliques no link")
     Impressions: int = Field(..., description="Número de impressões")
-    Conversions: Optional[int] = Field(None, description="Número de conversões")
+    Conversions: Optional[float] = Field(None, description="Número de conversões")
     Segmentação: str = Field(..., description="Tipo de segmentação utilizada")
     Tipo_de_Anúncio: str = Field(..., description="Tipo do anúncio")
     Fase: str = Field(..., description="Fase do lançamento ou campanha")
